d3e935edbade9cf85edfe822da7e4fc610215e7b
Hement Sharma <hement.ece@gmail.com>
2016-12-25 10:10:56 +0100

    Updated project
    updated project as per suggestion

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,35 +1,34 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+ This is empty on purpose! Your code to build the resume will go here.
  */
 /* Bio object contains personal information like name , role , contact etc*/
 bio = {
     name: "Hement Sharma",
     role: "Web Developer",
-    contact:{
+    contact: {
         mobile: "+91 8600059150",
         email: "hementsharma@outlook.com",
-        gitUrl: "https://github.com/HementSharma",
+        github: "https://github.com/HementSharma",
         location: "pune"
     },
     welcomeMessage: "",
     biopic: "images/fry.jpg",
     skills: ["HTML", "JavaScript", "CSS3", "Bootstrap", "Jquery"]
- }
+};
 
-//displayBio function is used to display the bio object's data on Resume page
-bio.displayBio = function (){
-    $("#header").append(HTMLbioPic.replace("%data%",bio.biopic));
+//display function is used to display the bio object's data on Resume page
+bio.display = function() {
+    $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
     $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
     $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-    $("#topContacts").append(HTMLmobile.replace("%data%", bio.contact.mobile) + HTMLemail.replace("%data%", bio.contact.email) + HTMLgithub.replace("%data%", bio.contact.gitUrl));
-    if(bio.skills.length >0     ){
+    $("#topContacts, #footerContacts").append(HTMLmobile.replace("%data%", bio.contact.mobile) + HTMLemail.replace("%data%", bio.contact.email) + HTMLgithub.replace("%data%", bio.contact.github) + HTMLlocation.replace("%data%", bio.contact.location));
+    if (bio.skills.length > 0) {
         $("#header").append(HTMLskillsStart);
-        for(i=0; i<bio.skills.length; i++){
-            $("#skills").append(HTMLskills.replace("%data%",bio.skills[i]));
-            $("#footerContacts").append(HTMLskills.replace("%data%",bio.skills[i]));
+        for (i = 0; i < bio.skills.length; i++) {
+            $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
         }
     }
-}
+};
 
 /* education object contains education details*/
 var education = {
@@ -37,69 +36,81 @@ var education = {
         name: "G.G.S.I.P.U",
         location: "New Delhi India",
         degree: "B.Tech. in Electronics and Communication",
-        major: "Electronics and Communication",
-        date: "2008 to 2012"
-    },
-        {
-            name: "Rajkiya Pratibha vikas vidyalaya",
-            location: "New Delhi India",
-            degree: "Science degree",
-            major: "Computer science",
-            date: "2006 to 2008"
-        }],
+        major: ["Electronics and Communication"],
+        dates: "2008 - 2012",
+        url: "http://www.ipu.ac.in/"
+    }, {
+        name: "Rajkiya Pratibha vikas vidyalaya",
+        location: "New Delhi India",
+        degree: "Science degree",
+        major: ["Computer science"],
+        dates: "2006 - 2008",
+        url: "https://en.wikipedia.org/wiki/Rajkiya_Pratibha_Vikas_Vidyalaya"
+    }],
 
-    onlineEducation:[{
-    class: "Front End Developer",
-    title: "Front End Developer",
-    school: "udacity",
-    url: "URL"
-}]
+    onlineCourses: [{
+        title: "Front End Developer",
+        school: "udacity",
+        dates: "2016-2017",
+        url: "https://www.udacity.com/"
+    }]
 
 };
 
 //below function will be used to display education details
 
-education.displayEdu = function(){
+education.display = function() {
 
-    education.schools.forEach(function(school){
+    education.schools.forEach(function(school) {
+        var HTMLschoolMajorlocal = "";
+        school.major.forEach(function(major) {
+            HTMLschoolMajorlocal = HTMLschoolMajorlocal + HTMLschoolMajor.replace("%data%", school.major);
+        })
         $("#education").append(HTMLschoolStart);
-        var HTMLschoolNamelocal = HTMLschoolName.replace("%data%", school.name);
-        var HTMLschoolDegreelocal = HTMLschoolDegree.replace("%data%" ,  school.degree);
-        var HTMLschoolDateslocal = HTMLschoolDates.replace("%data%",  school.date);
-        var HTMLschoolLocationlocal = HTMLschoolLocation.replace("%data%",  school.location);
-        var HTMLschoolMajorlocal = HTMLschoolMajor.replace("%data%" , school.major);
-        $(".education-entry:last-child").append(HTMLschoolNamelocal + HTMLschoolDegreelocal + HTMLschoolDateslocal + HTMLschoolLocationlocal+ HTMLschoolMajorlocal);
+        var HTMLschoolNamelocal = HTMLschoolName.replace("%data%", school.name).replace("#", school.url);
+        var HTMLschoolDegreelocal = HTMLschoolDegree.replace("%data%", school.degree);
+        var HTMLschoolDateslocal = HTMLschoolDates.replace("%data%", school.dates);
+        var HTMLschoolLocationlocal = HTMLschoolLocation.replace("%data%", school.location);
+        $(".education-entry:last-child").append(HTMLschoolNamelocal + HTMLschoolDegreelocal + HTMLschoolDateslocal + HTMLschoolLocationlocal + HTMLschoolMajorlocal);
     });
-}
+    $("#education").append(HTMLonlineClasses);
+    education.onlineCourses.forEach(function(onlineschool) {
+        $("#education").append(HTMLschoolStart);
+        HTMLonlineTitlelocal = HTMLonlineTitle.replace("%data%", onlineschool.title);
+        HTMLonlineSchoollocal = HTMLonlineSchool.replace("%data%", onlineschool.school);
+        HTMLonlineDateslocal = HTMLonlineDates.replace("%data%", onlineschool.dates);
+        HTMLonlineURLlocal = HTMLonlineURL.replace("%data%", onlineschool.url);
+        $(".education-entry:last-child").append(HTMLonlineTitlelocal + HTMLonlineSchoollocal + HTMLonlineDateslocal + HTMLonlineURLlocal);
+    });
+};
 
 /*Work object contains work information*/
-var work ={
-    jobs:[{
-        Employer:"Accenture",
+var work = {
+    jobs: [{
+        employer: "Accenture",
         title: "SAP BASIS Administrator",
-        location:"Pune",
-        dates:"In Progress",
-        description:"Working as a SAP BASIS consultant. My Role includes creation and maintinance of SAP servers and making sure that they work fine for business"
-    },
-    {
-        Employer:"udacity",
+        location: "Pune",
+        dates: "In Progress",
+        description: "Working as a SAP BASIS consultant. My Role includes creation and maintinance of SAP servers and making sure that they work fine for business"
+    }, {
+        employer: "udacity",
         title: "Front End Developer",
-        location:"Pune",
-        dates:"In Progress",
-        description:"Learning Front End Development to become a Web deveoper"
+        location: "Pune",
+        dates: "In Progress",
+        description: "Learning Front End Development to become a Web deveoper"
     }]
 };
 
 //display Work function will display the work details to the page
-work.displayWork = function(){
-    work.jobs.forEach(function(job){
-            $("#workExperience").append(HTMLworkStart);
-            var HTMLworkEmployerLocal = HTMLworkEmployer.replace("%data%",job.Employer);
-            var HTMLworkTitleLocal = HTMLworkTitle.replace("%data%", job.title);
-            var HTMLworkDateslocal = HTMLworkDates.replace("%data%", job.dates);
-            var HTMLworkLocationlocal = HTMLworkLocation.replace("%data%", job.location);
-            var HTMLworkDescriptionlocal = HTMLworkDescription.replace("%data%" , job.description);
-            $(".work-entry:last").append(HTMLworkEmployerLocal + HTMLworkTitleLocal + HTMLworkDateslocal + HTMLworkLocationlocal +HTMLworkDescriptionlocal);
+work.display = function() {
+    work.jobs.forEach(function(job) {
+        $("#workExperience").append(HTMLworkStart);
+        var HTMLworkEmployerLocal = HTMLworkEmployer.replace("%data%", job.employer);
+        var HTMLworkTitleLocal = HTMLworkTitle.replace("%data%", job.title);
+        var HTMLworkDateslocal = HTMLworkDates.replace("%data%", job.dates);
+        var HTMLworkLocationlocal = HTMLworkLocation.replace("%data%", job.location);
+        var HTMLworkDescriptionlocal = HTMLworkDescription.replace("%data%", job.description);
+        $(".work-entry:last").append(HTMLworkEmployerLocal + HTMLworkTitleLocal + HTMLworkDateslocal + HTMLworkLocationlocal + HTMLworkDescriptionlocal);
 
 
     });
@@ -107,35 +118,37 @@ work.displayWork = function(){
 
 /* projects object contain project details for the user*/
 var projects = {
-    projects:[{ title:"Portfolio Site",
-        date: "Dec 2016",
-        description : "This is a protfolio site project containing all skills and previous work.",
-        image:"images/project_image.jpg"
+    projects: [{
+        title: "Portfolio Site",
+        dates: "Dec 2016",
+        description: "This is a protfolio site project containing all skills and previous work.",
+        image: ["images/project_image.jpg"]
     }]
 };
 
 //display porjects will be used to display Project details
-projects.displayProjects  = function(){
-    projects.projects.forEach(function(project){
-        if(typeof projects[i] == 'object'){
-            var HTMLprojectTitlelocal = HTMLprojectTitle.replace("%data%",project.title);
-            var HTMLprojectDateslocal = HTMLprojectDates.replace("%data%",project.date);
+projects.display = function() {
+    projects.projects.forEach(function(project) {
+        if (typeof projects == 'object') {
+            var HTMLprojectImagelocal = "";
+            project.image.forEach(function(image) {
+                HTMLprojectImagelocal = HTMLprojectImagelocal + HTMLprojectImage.replace("%data%", image);
+            });
+            var HTMLprojectTitlelocal = HTMLprojectTitle.replace("%data%", project.title);
+            var HTMLprojectDateslocal = HTMLprojectDates.replace("%data%", project.dates);
             var HTMLprojectDescriptionlocal = HTMLprojectDescription.replace("%data%", project.description);
-            var HTMLprojectImagelocal = HTMLprojectImage.replace("%data%" , project.image);
             $("#projects").append(HTMLprojectStart);
             $(".project-entry:last-child").append(HTMLprojectTitlelocal + HTMLprojectDateslocal + HTMLprojectDescriptionlocal + HTMLprojectImagelocal);
         }
     });
-}
+};
 
 
-bio.displayBio();
-work.displayWork();
-projects.displayProjects();
-education.displayEdu();
+bio.display();
+work.display();
+projects.display();
+education.display();
 
 //$("#main").append(internationalizeButton);
 
 $("#mapDiv").append(googleMap);
-
-
